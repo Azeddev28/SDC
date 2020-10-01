c8020f64b652afd1b83e1e70b69430115e0122b1
Azeddev28 <ahsan.zh28@gmail.com>
2020-10-01 18:14:42 +0500

    "Configured database to postgresql"

--- config/settings.py
+++ config/settings.py
@@ -86,10 +86,11 @@ AUTH_USER_MODEL = 'apis.User'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sdc_database',
     }
+    
 }
 
 
