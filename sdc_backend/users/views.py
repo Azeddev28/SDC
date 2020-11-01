1b82381ba61a5e415cd0f77c1152d5e9c87b2777
Azeddev28 <ahsan.zh28@gmail.com>
2020-11-01 14:32:44 +0500

    "faulty import removed"

--- sdc_backend/users/views.py
+++ sdc_backend/users/views.py
@@ -16,7 +16,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework import response, decorators, permissions, status
-from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserCreateSerializer
 
 
